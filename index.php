f6d3e4784ffbf8b24ca55fe7edcbc6d9cd9c9bf1
klapaucius4 <klapaucius.mk@gmail.com>
2020-12-02 20:11:06 +0100

    detecting ip

--- index.php
+++ index.php
@@ -7,19 +7,36 @@ You should use also complicated and long value for your safety */
 
 header("HTTP/1.1 301 Moved Permanently");
 
+$fileName = "rpi-ddns-data.json";
+
 if(isset($_GET['auth'])){
     if(strip_tags($_GET['auth']) == AUTH_SALT){
         
         $ipAddress = getRealIpAddr();
+
         $json_data = json_encode(array('ip' => $ipAddress));
-        // $file = fopen("rpi-ddns-data.json", "r");
-        file_put_contents('rpi-ddns-data.json', $json_data);
+
+        $file = fopen($newfile, 'w') or die("Can't create file rpi-ddns-data.json");
+
+        if(file_put_contents('rpi-ddns-data.json', $json_data)){
+            echo 'Successfully saved IP: '.$ipAddress.'.';
+        }else{
+            echo 'Can \'t save IP.';
+        }
     }
 }else{
+
+    if($file = fopen($newfile, 'r')){
+        $data = json_encode($file);
+
+        var_dump($data);
+    }
     // otherwise do any action, for example redirect to Google
     header("Location: https://google.com/");
     // or echo some text, for example:
     // echo "Lorem ipsum dolor...";
+
+    
 }
 
 
