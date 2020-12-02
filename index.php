bcaf435c63eed18f4242445b849f9e99b03f74ab
klapaucius4 <klapaucius.mk@gmail.com>
2020-12-02 20:26:38 +0100

    mods

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(isset($_GET['auth'])){
         
         $ipAddress = getRealIpAddr();
 
-        $json_data = json_encode(array('ip' => $ipAddress, 'last_modyfication' => date()));
+        $json_data = json_encode(array('ip' => $ipAddress, 'last_modyfication' => date('now')));
 
         $fileHandle = fopen($fileName, 'w') or die("Can't create file rpi-ddns-data.json");
 
